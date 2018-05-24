a0a0414c863c9bc415ca7cd346f9d5dd5b16edef
Stephen Afolabi <stephenafolabi@gmail.com>
2018-05-24 12:14:00 +0000

    quiz updated

--- readfile.py
+++ readfile.py
@@ -29,13 +29,13 @@
 # print(most_common)
 #-------------------------------------5 character words 10 commonly used words-----
 
-# import re
-# import collections
-# text = open('1155-0.txt').read().lower()
-# words = re.findall('\w+', text)
-# long_words = []
-# for word in words:
-#     if len(word) >= 5:
-#         long_words.append(word)
-# most_common = collections.Counter(long_words).most_common(10)
-# print(most_common)
+import re
+import collections
+text = open('1155-0.txt').read().lower()
+words = re.findall('\w+', text)
+long_words = []
+for word in words:
+    if len(word) >= 5:
+        long_words.append(word)
+most_common = collections.Counter(long_words).most_common(10)
+print(most_common)
